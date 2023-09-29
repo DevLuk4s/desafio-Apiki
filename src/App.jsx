7c8436586f9e7eb1cac22959cf900acdec8feca2
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-09-29 16:49:53 -0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './componentes/Navbar/Navbar';
+import Home from './componentes/Home/Home';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
