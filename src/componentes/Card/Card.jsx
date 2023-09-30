22387a6f6d6405c0ff5e967e4713f24ac43a1f97
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-09-30 19:27:18 -0300

    atualizado react

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import './Card.css';
 
 function Card() {
 
+  const [card, setCard] = useState([]);
+
+  useEffect(() => {
+    fetch('https://exemplo.com/api/dados')
+      .then((response) => response.json())
+      .then((data) => {
+        setCard(data);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar dados da API:', error);
+      });
+  }, []);
+
   return (
-    <section className="card">
-      <div>
-        <img className="card__image" src="#" alt="" />
-        <h1 className="card__tittle">Tittle</h1>
-        <p className="card__paragrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, atque itaque. Aperiam repellendus sunt architecto exercitationem laudantium, reprehenderit quaerat nemo.</p>
-      </div>
-    </section>
+    <div>
+      {card.map(
+        
+      )}
+    </div>
   );
 }
 
