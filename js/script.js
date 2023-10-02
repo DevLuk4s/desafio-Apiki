ddbc10b86872783f2c56baeb3ec515bbbdd2b560
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-02 18:58:51 -0300

    desafio apiki

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-const card__tittle = document.querySelector(".card__tittle")
-const card__paragrafo = document.querySelector(".card__paragrafo")
-const card__link = document.querySelector(".card__link")
-const card__image = document.querySelector(".card__image")
+const card = document.querySelector(".container");
 
 const fatchAPI = async () => {
-    const res = await fetch("https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518")
-    const data = await res.json()
-    console.log(data)
+  const res = await fetch(
+    "https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518"
+  );
+  const data = await res.json();
+  console.log(data);
+  data.map((el, chave) => {
+    const container__texto = document.createElement("div");
+    container__texto.classList.add("texto");
+
+    const container__image = document.createElement("div");
+    container__texto.classList.add("image");
+
+    const card__image = document.createElement("img");
+    card__image.classList.add("card__image");
     card__image.src =
-}
+      data[chave]._embedded[
+        "wp:featuredmedia"
+      ][0].media_details.sizes.medium.source_url;
 
-fatchAPI()
+    const card__tittle = document.createElement("h1");
+    card__tittle.classList.add("card__titulo");
+    card__tittle.innerHTML = data[chave].title.rendered;
 
-// data[0]._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url
+    container__image.appendChild(card__image);
+    container__texto.appendChild(container__texto);
+    card.appendChild(container__image);
+    card.appendChild(container__texto);
+  });
+};
+
+fatchAPI();
