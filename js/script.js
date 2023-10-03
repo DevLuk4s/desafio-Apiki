1a36235d1773d3b0683a3904c7ea8070dc48f0c7
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-03 16:10:22 -0300

    desafio apiki

--- js/script.js
+++ js/script.js
@@ -1,34 +1,53 @@
-const card = document.querySelector(".container");
+const card = document.querySelector(".card");
 
-const fatchAPI = async () => {
+const fetchAPI = async () => {
   const res = await fetch(
     "https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518"
   );
   const data = await res.json();
+  return data;
+};
+
+const renderAPI = async () => {
+  const data = await fetchAPI();
   console.log(data);
-  data.map((el, chave) => {
+  data.map((el, id) => {
     const container__texto = document.createElement("div");
     container__texto.classList.add("texto");
 
     const container__image = document.createElement("div");
-    container__texto.classList.add("image");
+    container__image.classList.add("image");
+
+    const container = document.createElement("div");
+    container.classList.add("container");
 
     const card__image = document.createElement("img");
     card__image.classList.add("card__image");
     card__image.src =
-      data[chave]._embedded[
+      data[id]._embedded[
         "wp:featuredmedia"
       ][0].media_details.sizes.medium.source_url;
 
-    const card__tittle = document.createElement("h1");
-    card__tittle.classList.add("card__titulo");
-    card__tittle.innerHTML = data[chave].title.rendered;
+    const card__titulo = document.createElement("h1");
+    card__titulo.classList.add("card__titulo");
+    card__titulo.innerHTML = data[id].title.rendered;
+
+    const card__paragrafo = document.createElement("p");
+    card__paragrafo.classList.add("card__paragrafo");
+    card__paragrafo.innerHTML = data[id].excerpt.rendered;
+
+    const card__data = document.createElement("span");
+    card__data.classList.add("card__data");
+    card__data.innerHTML = data[id].date;
 
     container__image.appendChild(card__image);
-    container__texto.appendChild(container__texto);
-    card.appendChild(container__image);
-    card.appendChild(container__texto);
+    container__texto.appendChild(card__titulo);
+    container__texto.appendChild(card__data);
+    container__texto.appendChild(card__paragrafo);
+    container.appendChild(container__image);
+    container.appendChild(container__texto);
+    card.appendChild(container);
   });
 };
 
-fatchAPI();
+renderAPI();
--- js/script.ts
+++ js/script.ts
@@ -1,7 +1,4 @@
-const card__tittle = document.querySelector(".card__tittle");
-const card__paragrafo = document.querySelector(".card__paragrafo");
-const card__link = document.querySelector(".card__link");
-const card__image = document.querySelector(".card__image");
+const card = document.querySelector(".container");
 
 const fatchAPI = async () => {
   const res = await fetch(
@@ -9,13 +6,29 @@ const fatchAPI = async () => {
   );
   const data = await res.json();
   console.log(data);
-  card__image.src =
-    data[0]._embedded[
-      "wp:featuredmedia"
-    ][0].media_details.sizes.thumbnail.source_url;
-  card__tittle.innerHTML = data[0].title.rendered;
-  card__paragrafo.innerHTML = data[0].excerpt.rendered;
-  card__link.innerHTML = data[0].link;
+  data.map((el, id) => {
+    const container__texto = document.createElement("div");
+    container__texto.classList.add("texto");
+
+    const container__image = document.createElement("div");
+    container__image.classList.add("image");
+
+    const card__image = document.createElement("img");
+    card__image.classList.add("card__image");
+    card__image.src =
+      data[id]._embedded[
+        "wp:featuredmedia"
+      ][0].media_details.sizes.medium.source_url;
+
+    const card__titulo = document.createElement("h1");
+    card__titulo.classList.add("card__titulo");
+    card__titulo.innerHTML = data[id].title.rendered;
+
+    container__image.appendChild(card__image);
+    container__texto.appendChild(card__titulo);
+    card.appendChild(container__image);
+    card.appendChild(container__texto);
+  });
 };
 
 fatchAPI();
